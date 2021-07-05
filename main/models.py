b0f2efcf0945e84dde1b4498f3a3b6a2ad84c988
ivan.delpozo@kinanalytics.com <Kin@nalytics.2020>
2021-07-05 11:45:53 -0500

    Update

--- main/models.py
+++ main/models.py
@@ -26,9 +26,29 @@ class UserManager(models.Manager):
             errors['pw_match'] = "Password must match!"
         return errors       
 
-# BookManager
-# AuthorManager
-# ReviewManager
+class BookManager(models.Manager):
+    def book_validator(self, postData):
+        errors = {}
+        if len(postData['title']) < 2:
+            errors['title'] = "Invalid title. Title must be at least 3 characters"
+        return errors 
+
+class AuthorManager(models.Manager):
+    def author_validator(self, postData):
+        errors = {}
+        if len(postData['author_name']) < 2 or len(postData['author_name']) > 50:
+            errors['author_name'] = "Invalid name. Name must be at least 3 characters"
+        author_in_db = Author.objects.filter(name=postData['author_name'])
+        if len(author_in_db) >= 1:
+            errors['duplicate'] = 'Author already exists in our DB.'
+        return errors  
+
+class ReviewManager(models.Manager):
+    def review_validator(self, postData):
+        errors = {}
+        if len(postData['content']) < 2:
+            errors['content'] = "Invalid content. Content must be at least 3 characters"
+        return errors 
 
 # Julio, Pablo, Sergio, Marcelo, Luisa, Luis, Alexis
 
@@ -46,14 +66,14 @@ class Book(models.Model):
     title = models.CharField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    objects = UserManager()
+    objects = BookManager()
 
 class Author(models.Model):
     name = models.CharField(max_length=75)
     books = models.ManyToManyField(Book, related_name="authors")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    objects = UserManager()
+    objects = AuthorManager()
 
 class Review(models.Model):
     content = models.TextField()
@@ -62,4 +82,4 @@ class Review(models.Model):
     book_reviewed = models.ForeignKey(Book, related_name="book_reviews", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    objects = UserManager()
+    objects = ReviewManager()
--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 import re
+from django.http import request
 from django.shortcuts import redirect, render
 from django.contrib import messages
-from .models import Book, Review, User
+from .models import Author, Book, Review, User
 import bcrypt
 
 # Create your views here.
@@ -51,7 +52,9 @@ def logout(request):
     return redirect('/')
 
 def dashboard(request):
-    # if (logged_user)
+    if 'logged_user' not in request.session:
+        messages.error(request, "Please register or please log in first")
+        return redirect('/')
 
     context = {
         'logged_user' : User.objects.get(id=request.session['logged_user']),
@@ -60,7 +63,97 @@ def dashboard(request):
     }
     return render(request, 'dashboard.html', context)
 
-# def create_book(request):
+def create_book(request):
+    if 'logged_user' not in request.session:
+        messages.error(request, "Please register or please log in first")
+        return redirect('/')
+    if request.method == "POST":
+        book_errors = Book.objects.book_validator(request.POST)
+        review_errors = Review.objects.review_validator(request.POST)
+        errors = list(book_errors.values())+list(review_errors.values())
+
+        if request.POST['author_dropdown'] == "-1":
+            if request.POST['author_name'] == "":
+                messages.error(request, "Please choose an author from the dropdown or create a new one")
+            else:
+                    author_errors = Author.objects.author_validator(request.POST)
+                    errors += list(author_errors.values())
+
+        if errors:
+                for error in errors:
+                    messages.error(request, error)
+                return redirect ('/book/book_form')
+
+        if request.POST['author_dropdown'] == "-1":
+            author = Author.objects.create(name = request.POST['author_name'])
+        else:
+            author = Author.objects.get(id = request.POST['author_dropdown'])
+
+        this_book = Book.objects.create(title = request.POST['title'])
+        user = User.objects.get(id = request.session['logged_user'])
+        review = Review.objects.create(content = request.POST['content'], rating = int(request.POST['rating']), book_reviewed = this_book, user_review = user)
+        this_book.authors.add(author)
+        return redirect(f'/book/{this_book.id}')
+    return redirect('book/book_form')
+
+def book_form(request):
+    if 'logged_user' not in request.session:
+        messages.error(request, "Please register or please log in first")
+        return redirect('/')
+
+    context = {
+        'authors': Author.objects.all()
+    }
+    return render(request, 'add_book.html', context)
+
+def show_book(request, book_id):
+    if 'logged_user' not in request.session:
+        messages.error(request, "Please register or please log in first")
+        return redirect('/')
+
+    context = {
+        'book' : Book.objects.get(id=book_id)
+    }
+    return render(request, 'one_book.html', context)
+
+def add_review(request):
+    if 'logged_user' not in request.session:
+        messages.error(request, "Please register or please log in first")
+        return redirect('/')
+
+    if request.method == "POST":
+        book = Book.objects.get(id=request.POST['book_reviewed'])
+        errors = Review.objects.review_validator[request.POST]
+
+        if errors:
+            for key,value in errors.items():
+                messages.error(request, value)
+            return redirect(f'/book/{book.id}')
+
+        user = User.objects.get(id=request.session['logged_user'])
+        review = Review.objects.create(content = request.POST['content'], rating = int(request.POST['rating'], book_reviewed = book, user_review = user))
+        return redirect(f'/book/{book.id}')
+
+def user_page(request, user_id):
+    if 'logged_user' not in request.session:
+        messages.error(request, "Please register or please log in first")
+        return redirect('/')
+    user = User.objects.get(id=user_id)
+
+    context = {
+        'one_user': user
+    }
+    return render(request, 'user_page.html', context)
+
+def delete_review(request, review_id):
+    if 'logged_user' not in request.session:
+        messages.error(request, "Please register or please log in first")
+        return redirect('/')
+    review = Review.objects.get(id=review_id)
+    review.delete()
+    return redirect(f'/book/{review.book_reviewed.id}')
+
+
 # def Book_form(request):
 # def show_book(request, book_id):
 # def add_review(request):
